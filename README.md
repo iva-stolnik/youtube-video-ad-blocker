8c1721a2b8627b41ed00ae8cc33a118338facf8a
Iva Stolnik <iva.stolnik91@gmail.com>
2024-05-15 12:04:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# YouTube Video Ad Blocker
+
+## Overview
+YouTube video Ad Blocker is a Chrome extension that automatically skips YouTube video ads for a better viewing experience.
+
+## Installation
+- Clone this repository to your local machine.
+- Load the Extension in Chrome:
+    - Open Chrome and go to `chrome://extensions/`.
+    - Enable "Developer mode" using the toggle switch in the top right.
+    - Click on "Load unpacked" and select the directory where you cloned your repository.
+
+
+## Usage
+- The extension will automatically mute and skip ads on YouTube.
+
+## Privacy
+This extension does not collect any personal data.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
---
+++ content.js
@@ -0,0 +1,70 @@
+function handleMutations(mutationsList) {
+    mutationsList.forEach(mutation => {
+        if (mutation.type === 'childList') {
+            replaceAds();
+        }
+    });
+}
+
+function replaceAds() {
+    // Detect the ad preview element
+    const adPreview = document.querySelector('div.ytp-preview-ad');
+    const video = document.querySelector('video'); // Target ad video specifically
+   console.log(adPreview);
+    if(video && !video.src)
+    {
+        video.style = "display:none;"
+    }
+    
+    if (adPreview && video) {
+        video.style = "display:none;"
+        video.muted = true;           
+
+        if (video.duration && video.currentTime) {
+            video.currentTime = video.duration - 1;
+        }
+    }
+   
+    // Detect and click all variations of the "Skip Ad" button
+    const skipButtons = document.querySelectorAll('.ytp-ad-skip-button, .ytp-skip-ad-button, .ytp-ad-skip-button-container, .ytp-ad-skip-button-slot, .ytp-ad-skip-button-icon, .ytp-ad-skip-button-text');
+    skipButtons.forEach(button => {
+        button.click();
+    });
+
+    // Remove ad overlay info containers
+    const adOverlays = document.querySelectorAll('.ytp-ad-player-overlay-layout__ad-info-container, .ytp-ad-player-overlay-layout__player-card-container');
+    adOverlays.forEach(overlay => {
+        overlay.parentNode.removeChild(overlay);
+    });
+}
+
+function handleInitialLoad() {
+    // Run replaceAds() immediately on page load to handle any ads already present
+    replaceAds();
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(handleMutations);
+    document.removeEventListener('DOMContentLoaded', handleInitialLoad);
+
+    // Start observing the target node for configured mutations
+    // Wait until document.body is available
+    const observeBody = () => {
+        if (document.body) {
+            observer.observe(document.body, { childList: true, subtree: true });
+        } else {
+            requestAnimationFrame(observeBody);
+        }
+    };
+    
+    observeBody();
+}
+
+
+// Ensure handleInitialLoad is called as early as possible
+if (document.readyState === 'loading') {  // Loading hasn't finished yet
+    document.addEventListener('DOMContentLoaded', handleInitialLoad);
+
+} else {  // `DOMContentLoaded` has already fired
+    handleInitialLoad();
+}
+
